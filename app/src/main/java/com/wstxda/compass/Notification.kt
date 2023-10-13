383affde7cecef6bc4a45b932823934fa36e2e4a
WSTxda <wstxda@gmail.com>
2023-10-13 18:41:27 -0300

    Improvement notification service

--- app/src/main/java/com/wstxda/compass/Notification.kt
+++ app/src/main/java/com/wstxda/compass/Notification.kt
@@ -8,26 +8,21 @@ import android.content.Intent
 import android.provider.Settings
 import androidx.core.app.NotificationCompat
 
-const val CHANNEL_ID = "COMPASS_CHANNEL"
+private const val CHANNEL_ID = "COMPASS_CHANNEL"
 const val NOTIFICATION_ID = 1
 
 fun TileService.channel() = NotificationChannel(
-    CHANNEL_ID,
-    getString(R.string.channel_name),
-    NotificationManager.IMPORTANCE_LOW
+    CHANNEL_ID, getString(R.string.channel_name), NotificationManager.IMPORTANCE_LOW
 )
 
-fun TileService.notification(): Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-    .setSmallIcon(R.drawable.ic_notification)
-    .setContentTitle(getString(R.string.notification_title))
-    .setContentText(getString(R.string.notification_label))
-    .setContentIntent(notificationClickIntent())
-    .build()
+fun TileService.notification(): Notification =
+    NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentIntent(notificationClickIntent()).build()
 
 
 fun TileService.notificationClickIntent(): PendingIntent {
     val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
-        putExtra(Settings.EXTRA_APP_PACKAGE, BuildConfig.APPLICATION_ID)
+        putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
         addCategory(Intent.CATEGORY_DEFAULT)
         addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     }
