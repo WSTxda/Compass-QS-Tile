e50868e7b9b33d6cd0c637925b4c5cd79d90b0d7
WSTxda <wstxda@gmail.com>
2024-08-12 20:55:21 -0300

    Improvement notification service

--- app/src/main/java/de/klimek/compass/Notification.kt
+++ app/src/main/java/de/klimek/compass/Notification.kt
@@ -17,10 +17,12 @@ fun TileService.channel() = NotificationChannel(
     NotificationManager.IMPORTANCE_LOW
 )
 
-fun TileService.notification(): Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-    .setSmallIcon(R.drawable.ic_launcher_foreground)
-    .setContentIntent(notificationClickIntent())
-    .build()
+fun TileService.notification(): Notification =
+    NotificationCompat.Builder(this, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_notification)
+        .setContentTitle(getString(R.string.notification_title))
+        .setContentText(getString(R.string.notification_label))
+        .setContentIntent(notificationClickIntent()).build()
 
 
 fun TileService.notificationClickIntent(): PendingIntent {
