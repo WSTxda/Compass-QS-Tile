6e33719e0afe0a54c13814119573d4317f7fbce3
WSTxda <wstxda@gmail.com>
2025-09-17 01:40:45 -0300

    Not anim coin flip make result on click

--- app/src/main/java/com/wstxda/toolkit/tiles/coinflip/CoinFlipTileService.kt
+++ app/src/main/java/com/wstxda/toolkit/tiles/coinflip/CoinFlipTileService.kt
@@ -3,8 +3,6 @@ package com.wstxda.toolkit.tiles.coinflip
 import android.graphics.drawable.Icon
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
-import android.os.Handler
-import android.os.Looper
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.util.Log
@@ -19,8 +17,6 @@ private const val TAG = "CoinFlipTileService"
 
 class CoinFlipTileService : TileService() {
 
-    private val handler = Handler(Looper.getMainLooper())
-    private var isAnimating = false
     private lateinit var haptics: Haptics
 
     companion object {
@@ -45,43 +41,8 @@ class CoinFlipTileService : TileService() {
 
     override fun onClick() {
         Log.i(TAG, "Click")
-        if (isAnimating) return
         val flipResult = Random.nextBoolean()
-        updateTileAsFlipping(flipResult)
-    }
-
-    private fun updateTileAsFlipping(flipResult: Boolean) {
-        isAnimating = true
-        val frames = listOf(
-            CoinFlipIconFactory.getHeadsIcon(), CoinFlipIconFactory.getTailsIcon()
-        )
-        var frameIndex = 0
-        val maxFrames = Random.nextInt(10, 15)
-
-        fun updateTile(isHeads: Boolean) {
-            qsTile?.update {
-                state = Tile.STATE_ACTIVE
-                label = getString(R.string.coin_flippling_label)
-                icon = Icon.createWithResource(
-                    applicationContext, if (isHeads) frames[0] else frames[1]
-                )
-            }
-        }
-
-        val runnable = object : Runnable {
-            override fun run() {
-                if (frameIndex < maxFrames) {
-                    updateTile(frameIndex % 2 == 0)
-                    haptics.tick()
-                    frameIndex++
-                    handler.postDelayed(this, 150)
-                } else {
-                    updateTileWithResult(flipResult)
-                    isAnimating = false
-                }
-            }
-        }
-        handler.post(runnable)
+        updateTileWithResult(flipResult)
     }
 
     private fun updateTileWithResult(isHeads: Boolean) {
@@ -98,19 +59,17 @@ class CoinFlipTileService : TileService() {
                 subtitle = getString(R.string.coin_flip_count, headsCount, tailsCount)
             }
             icon = Icon.createWithResource(
-                applicationContext,
-                if (isHeads) CoinFlipIconFactory.getHeadsIcon() else CoinFlipIconFactory.getTailsIcon()
+                applicationContext, if (isHeads) CoinFlipIconFactory.getHeadsIcon()
+                else CoinFlipIconFactory.getTailsIcon()
             )
         }
         haptics.tick()
     }
 
     private fun updateTileAsInactive() {
-        isAnimating = false
-        handler.removeCallbacksAndMessages(null)
+        headsCount = 0
+        tailsCount = 0
         qsTile?.update {
-            headsCount = 0
-            tailsCount = 0
             state = Tile.STATE_INACTIVE
             label = getString(R.string.coin_flip_tile_label)
             if (VERSION.SDK_INT >= VERSION_CODES.Q) {
